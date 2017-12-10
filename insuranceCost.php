265622bf0c7279c10bd8ef1db46d214ba720986d
Randi Faustino Roxas <randito_roxasjr@dlsu.edu.ph>
2017-12-10 21:57:37 +0800

    update

--- insuranceCost.php
+++ insuranceCost.php
@@ -147,37 +147,37 @@
           <li class="nav-item">
             <div class="btn-group">
 <!----------------------------------------------------------------------------->
-              <!-- QUERY Notifications -->
-              <?php
-              # Connect to Database
-              require_once('home_Agent_mysqli_connect.php');
+                              <!-- QUERY Notifications -->
+                              <?php
+                              # Connect to Database
+                              require_once('home_Agent_mysqli_connect.php');
 
-              # query
-              $query = "SELECT COUNT(*) as 'NUM' FROM notification WHERE notifType < 2";
-              $result = mysqli_query($dbc, $query);
-              $data = mysqli_fetch_assoc($result);
-              ?>
-              <!-- NOTIFICATION BUTTON -->
-                  <button id="notifications" class="btn dropdown-toggle text-white" data-toggle="dropdown" style="cursor:pointer">
-                      <i style="color: #f42929"class="fa d-inline fa-lg fa-exclamation -o"></i>
-                          <span style="font-size: 18px; font-family: 'Roboto', sans-serif" class="w3-badge w3-red"><?php echo $data['NUM'];?></span>
-                               Notifications
-                  </button>
-              <div class="dropdown-menu">
+                              # query
+                              $query = "SELECT COUNT(*) as 'NUM' FROM notification WHERE notifType < 2 and isApproved = 0";
+                              $result = mysqli_query($dbc, $query);
+                              $data = mysqli_fetch_assoc($result);
 
-                <!-- NOTIFICATION Data -->
-                <?php
+                              ?>
 
-                $query1 = "SELECT * FROM employee e, notification n WHERE e.employeeId = n.employeeId and e.managedBy = $_SESSION[employeeId] and notifType < 2 and n.isApproved = 0 ORDER BY n.timeCreated";
-                $result1 = mysqli_query($dbc, $query1);
+                              <!-- NOTIFICATION BUTTON -->
+                                  <button id="notifications" class="btn dropdown-toggle text-white" data-toggle="dropdown" style="cursor:pointer">
+                                      <i style="color: #f42929"class="fa d-inline fa-lg fa-exclamation -o"></i>
+                                          <span style="font-size: 18px; font-family: 'Roboto', sans-serif" class="w3-badge w3-red"><?php echo $data['NUM'];?></span>
+                                               Notifications
+                                  </button>
+                              <div class="dropdown-menu">
+                              <!-- NOTIFICATION Data -->
+                              <?php
 
-                    #INCREMENT ID LOOP
-                    while($data1 = mysqli_fetch_assoc($result1)){
-                      echo '<a class="dropdown-item text-center"><center><a href="#">'.$data1["firstName"].' '.$data1["lastName"].'</a>'. ': '. $data1["message"]. '<a href="#"></a></center></a></a>';
-                      echo '<div class="dropdown-divider"></div>';
-                    }
-                ?>
-                </div>
+                              $query1 = "SELECT e.firstName, e.lastName, n.message, e.employeeId, n.employeeId, n.timeCreated, e.managedBy, e.isManager, n.notifType, n.isApproved FROM employee e, notification n WHERE e.employeeId = n.employeeId and e.managedBy = $_SESSION[employeeId] and notifType < 2 and n.isApproved = 0 ORDER BY n.timeCreated";
+                              $result1 = mysqli_query($dbc, $query1);
+
+                                  #INCREMENT ID LOOP
+                                  while($data1 = mysqli_fetch_assoc($result1)){
+                                    echo '<a class="dropdown-item text-center"><center><a href="#">'.$data1["firstName"].' '.$data1["lastName"].'</a>'. ': '. $data1["message"]. '<a href="#"></a></center></a></a>';
+                                    echo '<div class="dropdown-divider"></div>';
+                                  }
+                              ?>
 <!----------------------------------------------------------------------------->
 
 
@@ -200,7 +200,6 @@
 
       <?php
         # QUERY NEW CLIENT DETAILS
-        echo $_SESSION['client_Id'];
         $client_query = "SELECT email_address, firstName, lastName, postalCode,clientId, province, city, addressLine1, addressLine2, phoneNumber1, phoneNumber2 FROM client WHERE $_SESSION[client_Id] = clientId limit 1";
         $client_result = mysqli_query($dbc, $client_query);
         $client_data = mysqli_fetch_assoc($client_result);
